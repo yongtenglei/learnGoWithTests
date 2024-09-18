fcfcf99edc8c4809a68a678a76094709c514c75f
yongtenglei <yongtengrey@outlook.com>
2024-09-18 21:32:33 +0200

    More meaning for TDT

--- structs/shapes.go
+++ structs/shapes.go
@@ -15,6 +15,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) (perimeter float64) {
 	perimeter += 2 * (rectangle.Width + rectangle.Height)
 	return
@@ -26,4 +31,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
