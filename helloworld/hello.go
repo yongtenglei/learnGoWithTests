4a837e12ec435f4d8d0d47735436d6697cd76f6f
yongtenglei <yongtengrey@outlook.com>
2024-09-18 12:38:04 +0200

    TDD hello world

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
 	if len(name) == 0 {
-		return englishHelloPrefix + "World"
+		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Rey"))
+	fmt.Println(Hello("Rey", ""))
 }
