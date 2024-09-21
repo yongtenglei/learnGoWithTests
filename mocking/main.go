a3a91529d6130460600b8315d7998cc4dbd54370
yongtenglei <yongtengrey@outlook.com>
2024-09-21 18:13:24 +0200

    Added SpyCountdownOperations

--- mocking/main.go
+++ mocking/main.go
@@ -16,19 +16,29 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const (
+	write = "write"
+	sleep = "sleep"
+)
 
 func Countdown(writer io.Writer, sleeper Sleeper) {
 	for i := CountdownStart; i > 0; i-- {
