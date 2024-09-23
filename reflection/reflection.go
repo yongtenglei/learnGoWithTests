8ea29c7f67a329aa59529568b7835d44e24ad45e
yongtenglei <yongtengrey@outlook.com>
2024-09-23 22:59:50 +0200

    pointer

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,7 @@ package reflection
 import "reflect"
 
 func walk(x any, fn func(string)) {
-	v := reflect.ValueOf(x)
+	v := getValue(x)
 
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
@@ -15,4 +15,14 @@ func walk(x any, fn func(string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x any) reflect.Value {
+	v := reflect.ValueOf(x)
+
+	if v.Kind() == reflect.Pointer {
+		v = v.Elem()
+	}
+
+	return v
 }
